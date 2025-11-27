9179d4d06e8aca91c7a9cb2e3ffe1085b20e9a86
qichang.dong <dongqichang@ecmas.ai>
2025-11-28 07:17:03 +0800

    u

--- app/data_preprocess/check_train_file.py
+++ app/data_preprocess/check_train_file.py
@@ -8,82 +8,42 @@ out_dir = Path(__file__).parent / "out"
 out_dir.mkdir(parents=True, exist_ok=True)
 
 
-def read_parquet_to_dataframe(file_path):
-    """
-    Reads a Parquet file and converts it to a Pandas DataFrame.
-
-    Parameters:
-    file_path (str): The path to the Parquet file.
-
-    Returns:
-    pd.DataFrame: The resulting Pandas DataFrame.
-    """
-    # Read the Parquet file into a PyArrow Table
-    table = pq.read_table(file_path)
-
-    # Convert the PyArrow Table to a Pandas DataFrame
-    dataframe = table.to_pandas()
-    print(
-        f"DataFrame loaded with {len(dataframe)} rows and {len(dataframe.columns)} columns.\ndataframe.head():\n{dataframe.head()}"
-    )
-    print(f"{type(dataframe) = }")
-    for row_i, row in dataframe.iterrows():
-        print(row)
-        print(f"\n{type(row) = }")
-        blueberry_flowering = row.get("blueberry_flowering", None)
-        blueberry_flowering_labels = row.get("blueberry_flowering_labels", None)
-        print(f"{blueberry_flowering = }")
-        print(f"{type(blueberry_flowering) = }")
-        print(f"{blueberry_flowering_labels = }")
-        break  # Print only the first row for br
-
-    # for row in dataframe.itertuples():
-    #     print(row[:10])
-    #     break  # Print only the first row for brevity
-
-    # with open("dataframe_columns.log", "w") as f:
-    #     for column in dataframe.columns:
-    #         f.write(f"{column}\n")
-
-    return dataframe
-
-
 def read_img_parquet_to_dataframe(
-    file_path="/data/corpus/findableai/phenology/df_labels_and_images.parquet",
+    file_path="/data/corpus/ocr/findableai/phenology/df_labels_and_images.parquet",
 ):
-
     # Read the Parquet file into a PyArrow Table
     table = pq.read_table(file_path)
-
-    # Convert the PyArrow Table to a Pandas DataFrame
-    dataframe = table.to_pandas()
-    print(
-        f"DataFrame loaded with {len(dataframe)} rows and {len(dataframe.columns)} columns.\ndataframe.head():\n{dataframe.head()}"
-    )
-    for row_i, row in dataframe.iterrows():
-        print(f"{type(row) = }")
-        barley_maturing_time_image = row.get("barley_maturing_time_image", None)
-        barley_maturing_time_labels = row.get("barley_maturing_time_labels", None)
-        print(f"{type(barley_maturing_time_image) = }")
-        print(f"{type(barley_maturing_time_labels) = }, {barley_maturing_time_labels = }")
-
-        out_img_path = out_dir / "barley_maturing_time_image.png"
-        if barley_maturing_time_image is not None:
+    img_name = "barley_maturing_time"
+    for row in table.to_pylist():
+        print(f'{type(row) = }')
+        img = row.get(f"{img_name}_image", None)
+        labels = row.get(f"{img_name}_labels", None)
+        if img:
+            out_img_path = out_dir / f"{img_name}_image.png"
             with open(out_img_path, "wb") as img_file:
-                img_file.write(barley_maturing_time_image)
+                img_file.write(img)
             print(f"Sample image written to: {out_img_path}")
-            break  # Print only the first row for br
+            out_label_path = out_dir / f"{img_name}_labels.txt"
+            with open(out_label_path, "w", encoding="utf-8") as f:
+                if labels is not None:
+                    f.write(labels)
+            break  # Print only the first row for brevity
 
-    # for row in dataframe.itertuples():
-    #     print(row[:10])
-    #     break  # Print only the first row for brevity
-    log_file = out_dir / (Path(file_path).stem + ".log")
+    log_file = out_dir / (Path(file_path).stem + "_columns.log")
+    columns = table.schema.names
+    img_columns = [col for col in columns if col.endswith("_image")]
+    unique_img_columns = set(img_columns)
+    label_columns = [col for col in columns if col.endswith("_labels")]
+    unique_label_columns = set(label_columns)
+    columns_from_img = [col.removesuffix("_image") for col in img_columns]
+    columns_from_labels = [col.removesuffix("_labels") for col in label_columns]
+    common_columns = set(columns_from_img).intersection(set(columns_from_labels))
+    print(f'{len(common_columns) = }')
+    print(f'{len(img_columns) = }, {len(label_columns) = }')
+    print(f'{len(unique_img_columns) = }, {len(unique_label_columns) = }')
     with open(log_file, "w", encoding="utf-8") as f:
-        for column in dataframe.columns:
+        for column in table.schema.names:
             f.write(f"{column}\n")
 
-    return dataframe
 
-
-# read_parquet_to_dataframe("vlm_finetuning/data/df_complete.parquet")
 read_img_parquet_to_dataframe()
